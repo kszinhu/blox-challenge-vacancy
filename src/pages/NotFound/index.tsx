a6334a8821eb65394f43c9ad14c478bc61ae9b8f
Cassiano Rodrigues <66069923+kszinhu@users.noreply.github.com>
2022-09-18 15:17:28 -0300

    feat: add Not Found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+import { Container } from "./styles";
+
 export default function NotFound() {
-  return null;
+  return (
+    <Container spacing={2}>
+      <Stack spacing={2} alignItems='center'>
+        <Typography variant='h1' sx={{ fontWeight: "bold" }}>
+          404
+        </Typography>
+        <Typography variant='h4' component='h2' sx={{ fontSize: 18 }}>
+          The page you are looking for does not exist.
+        </Typography>
+      </Stack>
+      <Button variant='text' component={Link} to='/'>
+        Go to home
+      </Button>
+    </Container>
+  );
 }
---
+++ src/pages/NotFound/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Stack } from "@mui/material";
+
+const Container = styled(Stack)`
+  height: 100vh;
+  width: 100vw;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export { Container };
