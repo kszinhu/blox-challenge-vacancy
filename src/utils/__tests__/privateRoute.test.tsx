2fbed1af03c68aec7818125f08a6186f2bb54b30
Cassiano Rodrigues <66069923+kszinhu@users.noreply.github.com>
2022-09-18 12:27:11 -0300

    feat: add test for privateRoute

---
+++ src/utils/__tests__/privateRoute.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PrivateRoutes from "../PrivateRoute";
+import { AuthProvider } from "../../contexts/AuthContext";
+
+global.React = React;
+
+describe("<PrivateRoutes />", () => {
+  describe("when the user is authenticated", () => {
+    it("should render the fallback route", () => {
+      const FakeComponent = () => <div>fake component</div>;
+      const Login = () => <div>Sign In</div>;
+
+      render(
+        <AuthProvider>
+          <Router>
+            <Routes>
+              <Route path='/sign-in' element={<Login />} />
+              <Route path='/' element={<PrivateRoutes />}>
+                <Route path='/' element={<FakeComponent />} />
+              </Route>
+            </Routes>
+          </Router>
+        </AuthProvider>
+      );
+
+      expect(screen.getByText("Sign In")).toBeInTheDocument();
+    });
+  });
+});
